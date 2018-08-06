cd1d998a0ee1e93c2ca1b7068008434eb78cdcb1
Maxberman <mb3ax@virginia.edu>
2018-08-06 09:35:53 -0400

    db now tracks new accounts

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask,render_template
-#import os
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import scoped_session,sessionmaker
-
-"""engine = create_engine(os.getenv("postgres://bcdopqkuuoevky:509345ac532d766ae2c8d8\
-09d619bbed3cb2360b468d37cdad59c81793feb837@ec2-23-23-242-163.compute-1.amazonaws.com:5432/dcc19vkphh1oms"))
-db = scoped_session(sessionmaker(bind=engine))"""
+from flask import Flask,render_template,session,request
+from flask_session import Session
+import os
+#from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+#from sqlalchemy.orm import scoped_session, sessionmaker
 
 app=Flask(__name__)
+engine=create_engine('postgres://bcdopqkuuoevky:509345ac532d766ae2c8d809d619bbed3cb2360b468d37cdad59c81793feb837@ec2-23-23-242-163.compute-1.amazonaws.com:5432/dcc19vkphh1oms')
+db=engine.connect()
+#app.config['SQLALCHEMY_DATABASE_URI']='postgres://bcdopqkuuoevky:509345ac532d766ae2c8d809d619bbed3cb2360b468d37cdad59c81793feb837@ec2-23-23-242-163.compute-1.amazonaws.com:5432/dcc19vkphh1oms'
+#db=SQLAlchemy(app)
+
+Session(app)
 
 @app.route("/")
 def mainpage():
@@ -21,3 +25,20 @@ def crewpage():
 @app.route("/page4.html")
 def timespage():
     return render_template("page4.html")
+@app.route("/createaccount.html", methods=['GET','POST'])
+def createaccountpage():
+    return render_template("createaccount.html")
+@app.route("/testcreation" ,methods=['GET','POST'])
+def testcreation():
+    username=request.form.get("username")
+    password=request.form.get("passcode")
+    #print('form requests were accepted', file=std.out)
+    testresults=db.execute("SELECT * FROM users WHERE username= '"+username+"' AND password= '"+password +"';").fetchall()
+    #print('got past fetch',file=std.out)
+    if len(testresults) == 0:
+        #print('got past rowcount test, success',file=std.out)
+        db.execute("INSERT INTO users (username,password) VALUES ('"+username+"','"+password+"');")
+        return 'success! this username has not been taken'
+    else:
+        #print('got past rowcount test, fail',file=std.out)
+        return 'fail! this username has already been taken'
